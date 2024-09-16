fix tracer variance growth

convection-experiments/forced_convection_2D.py
@@ -9,6 +9,16 @@ import argparse
 from mpi4py import MPI
 import matplotlib.ticker as ticker
 
+# test case parameters
+domain_width = 10_000 # width of domain in metres
+domain_height = 10_000 # height of domain in metres
+
+p_surface = 1_00_000.0 # surface pressure in Pa
+SST = 300.0 # sea surface temperature in Kelvin
+
+cooling_rate = 10.0 / (3600 * 24) # cools 10 K per day
+
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
@@ -20,13 +30,10 @@ parser.add_argument('--nz', type=int, help='Number of cells in vertical')
 parser.add_argument('--nproc', type=int, help='Number of procs', default=1)
 parser.add_argument('--plot', action='store_true')
 args = parser.parse_args()
-#-6.80467525352385e-05
-# domain size
-xlim = 10_000
-zlim = 10_000
+
 # maps to define geometry these can be arbitrary - maps [0, 1]^2 to domain
-zmap = lambda x, z: z * zlim
-xmap = lambda x, z: xlim * (x - 0.5)
+zmap = lambda x, z: z * domain_height
+xmap = lambda x, z: domain_width * (x - 0.5)
 
 # number of cells in the vertical and horizontal direction
 nz = args.nz
@@ -40,98 +47,57 @@ poly_order = args.order # spatial order of accuracy
 a = 0.5 # kinetic energy dissipation parameter
 upwind = True
 
-SST  = 290.0
-
 # experiment name - change this for new experiments!
-exp_name_short = 'ice-bubble'
+exp_name_short = 'forced-convection'
 experiment_name = f'{exp_name_short}-nx-{nx}-nz-{nz}-p{poly_order}'
 data_dir = os.path.join('data', experiment_name)
 plot_dir = os.path.join('plots', experiment_name)
 
 if rank == 0:
-    print(f"---------- Ice bubble with nx={nx}, nz={nz}")
+    print(f"---------- {exp_name_short} with nx={nx}, nz={nz}")
     if not os.path.exists(plot_dir): os.makedirs(plot_dir)
     if not os.path.exists(data_dir): os.makedirs(data_dir)
 
 comm.barrier()
-#
-
-# def neutrally_stable_dry_profile(solver):
-#     p_surface = 1_00_000.0  # surface pressure in Pa
-#     T_surface = 300  # surface temperature in Kelvin
-#     # create a hydrostatically balanced pressure and density profile
-#     dexdy = -g / (solver.cpd * T_surface)
-#     ex = 1 + dexdy * solver.zs
-#     p = p_surface * ex ** (solver.cpd / solver.Rd)
-#     density = p / (solver.Rd * ex * T_surface)
-#
-#     return density, p
-#
-#
-# def initial_condition(solver, pert=0.0):
-#
-#     # initial wind is zero
-#     u = np.zeros_like(solver.xs)
-#     w = np.zeros_like(solver.xs)
-#
-#     density, p = neutrally_stable_dry_profile(solver)
-#
-#     # add arbitrary moisute profile
-#     qw = solver.rh_to_qw(0.95, p, density)  # choose 95% relative humidity
-#
-#     # model must be initialized with entropy not temperature
-#     # so convert density, pressure, qw profile to a density, entropy, qw profile
-#     s = solver.entropy(density, qw, p=p)
-#
-#     # add perturbation/bubble to profile
-#     # increase entropy
-#     bubble_radius = 2_000
-#     distance = np.sqrt(solver.xs ** 2 + (solver.zs - 1.0 * bubble_radius) ** 2)
-#     mask = distance < bubble_radius
-#     s += mask * pert * 3 * (np.cos(np.pi * (distance / bubble_radius) / 2) ** 2)
-#
-#     return u, w, density, s, qw
 
 
-def initial_condition(solver, pert):
-    # initial velocity is zero
-    u = np.zeros_like(solver.zs)
-    v = np.zeros_like(solver.zs)
-
-    dry_theta = 300
-    dexdy = -g / (solver.cpd * dry_theta)
+def neutrally_stable_dry_profile(solver):
+    # create a hydrostatically balanced pressure and density profile
+    dexdy = -g / (solver.cpd * SST)
     ex = 1 + dexdy * solver.zs
-    p = 1_00_000.0 * ex ** (solver.cpd / solver.Rd)
-    density = p / (solver.Rd * ex * dry_theta)
+    p = p_surface * ex ** (solver.cpd / solver.Rd)
+    density = p / (solver.Rd * ex * SST)
 
-    qw = solver.rh_to_qw(0.95, p, density)
-    qd = 1 - qw
-
-    R = solver.Rd * qd + solver.Rv * qw
-    T = p / (R * density)
-
-    assert (qw <= solver.saturation_fraction(T, density)).all()
-
-    rad_max = 2_000
-    rad = np.sqrt(solver.xs ** 2 + (solver.zs - 1.0 * rad_max) ** 2)
-    mask = rad < rad_max
-    density -= mask * (pert * density / 300) * (np.cos(np.pi * (rad / rad_max) / 2) ** 2)
-
-    T = p / (R * density)
-    assert (qw <= solver.saturation_fraction(T, density)).all()
-
-    s = qd * solver.entropy_air(T, qd, density)
-    s += qw * solver.entropy_vapour(T, qw, density)
-
-    return u, v, density, s, qw
+    return density, p
 
 
+def initial_condition(solver):
+    # initial wind is zero
+    u = np.zeros_like(solver.xs)
+    w = np.zeros_like(solver.xs)
+
+    density, p = neutrally_stable_dry_profile(solver)
+
+    # add arbitrary moisute profile
+    qw = solver.rh_to_qw(0.95, p, density)  # choose 95% relative humidity
+
+    # model must be initialized with entropy not temperature
+    # so convert density, pressure, qw profile to a density, entropy, qw profile
+    s = solver.entropy(density, qw, p=p)
+
+    # can also do s = solver.entropy(density, qw, T=T) to use temperature profiles
+
+    return u, w, density, s, qw
+
+
+# forcing - this adds uniform cooling to air
+# and forces the bottom temperature to match the SST
 def cooling_and_sst_forcing(solver, state, dstatedt):
     u, w, h, s, q, T, mu, p, ie = solver.get_vars(state)
     dudt, dwdt, dhdt, dsdt, dqdt, *_ = solver.get_vars(dstatedt)
 
     # internal cooling
-    T_forcing = -0.0 / (3600 * 24) # constantly cool at a rate of 1K per day
+    T_forcing = -cooling_rate * solver.zs / solver.zs.max() # constantly cool at a rate of 1K per day
     s_forcing = T_forcing * solver.cvd / T # convert temperature forcing to entropy forcing
 
     # boundary forcing at bottom - force bottom temperature  towards SST
@@ -144,7 +110,7 @@ def cooling_and_sst_forcing(solver, state, dstatedt):
 
 
 # total run time
-run_time = 600
+run_time = 3000
 
 # save data at these times
 tends = np.array([0.0, (1 / 3), (2 / 3), 1.0]) * run_time
@@ -153,30 +119,13 @@ time_list = []
 energy_list = []
 
 if run_model:
-    solver = FortranThreePhaseEuler2D(xmap, zmap, poly_order, nx, g=g, cfl=0.5, a=0.5, nz=nz, upwind=upwind, nprocx=nproc, forcing=None)
-    u, v, density, s, qw = initial_condition(solver, pert=2.0)
-    solver.set_initial_condition(u, v, density, s, qw)
-    # print("Bottom temp range:", solver.T[:, 0, :, 0].min(), solver.T[:, 0, :, 0].max())
+    solver = FortranThreePhaseEuler2D(xmap, zmap, poly_order, nx, g=g, cfl=0.5, a=0.5, nz=nz, upwind=upwind, nprocx=nproc, forcing=cooling_and_sst_forcing)
+    u, v, density, s, qw = initial_condition(solver)
 
-    # if rank == 0:
-    #     density = 0.6275959315151061;
-    #     entropy = 2531.0038776852075;
-    #     qw = 0.01330944126634543;
-    #     density = np.array([density]); entropy = np.array([entropy]); qw = np.array([qw]);
-    #     qv, ql, qi = solver.solve_fractions_from_entropy(density, qw, entropy)
-    #     print(qv, ql, qi)
-    #
-    #     pysolver = ThreePhaseEuler2D(xmap, zmap, poly_order, nx, g=g, cfl=0.5, a=0.5, nz=nz, upwind=upwind, nprocx=nproc, forcing=None)
-    #     density = 0.6275959315151061;
-    #     entropy = 2531.0038776852075;
-    #     qw = 0.01330944126634543;
-    #     density = np.array([density]);
-    #     entropy = np.array([entropy]);
-    #     qw = np.array([qw]);
-    #     qv, ql, qi = pysolver.solve_fractions_from_entropy(density, qw, entropy)
-    #     print(qv, ql, qi)
-    #
-    # exit(0)
+    np.random.seed(42 + rank)
+    noise = 2 * (np.random.random(density.shape) - 0.5)
+    density += 0.01 * density * noise
+    solver.set_initial_condition(u, v, density, s, qw)
 
     E0 = solver.energy()
     for i, tend in enumerate(tends):
@@ -206,7 +155,7 @@ elif rank == 0:
     #
     solver_plot = ThreePhaseEuler2D(xmap, zmap, poly_order, nx, g=g, cfl=0.5, a=a, nz=nz, upwind=upwind, nprocx=1)
     # base state of the initial condition (excludes bubble perturbation)
-    _, _, h0, s0, qw0 = initial_condition(solver_plot, pert=0.0)
+    _, _, h0, s0, qw0 = initial_condition(solver_plot)
     qv0, ql0, qi0 = solver_plot.solve_fractions_from_entropy(h0, qw0, s0)
 
     def fmt(x, pos):
@@ -242,9 +191,9 @@ elif rank == 0:
             im = solver_plot.plot_solution(ax, dim=2, plot_func=plot_fun)
             cbar = plt.colorbar(im, ax=ax, format=ticker.FuncFormatter(fmt))
             cbar.ax.tick_params(labelsize=8)
+            plt.tight_layout()
 
     for (fig, ax), label in zip(fig_list, labels):
         plot_name = f'{label}_{exp_name_short}'
         fp = solver_plot.get_filepath(plot_dir, plot_name, ext='png')
         fig.savefig(fp, bbox_inches="tight")
-

moist_euler_dg/two_phase_euler_2D.py
@@ -265,7 +265,7 @@ class TwoPhaseEuler2D(Euler2D):
         c_adv = np.abs(0.5 * (normal_vel_p + normal_vel_m))
         c_snd = 0.5 * (cp + cm)
 
-        F_num_flux = 0.5 * (Fp + Fm) #- self.a * (c_adv + c_snd) * (hp - hm) * norm_contra
+        F_num_flux = 0.5 * (Fp + Fm) - self.a * (c_adv + c_snd) * (hp - hm) * norm_contra
 
         fluxp = Gp
         fluxm = Gm